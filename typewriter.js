bddd99a56afb9c858a7e21ac702ccb451f9140f5
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-11 18:13:33 -0400

    typer_writer_LightHouseLabs

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let pause = 0; // so the first letter to be shown instant
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, pause);
+  pause += 50;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, pause);
